7f7cae865de140fcb80c147053af275536d81d66
Jose_padilla <jcpadilla@uniguajira.edu.co>
2020-09-16 21:31:40 -0500

    creamos la clase alumno - hecho por Padilla

---
+++ ejercicio_01.py
@@ -0,0 +1,8 @@
+#Ejercicio Alumno que aprobó o no aprobó
+
+#primero se crea la clase alumno e inicimos el atributo 
+
+class Alumno:
+    def inicializar(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
